f50d5d6142671ccea51a7c227f99c4f5b008ef22
amira <amira.a.a.hashim@gmail.com>
2020-09-21 20:36:05 +0200

    login/logout

--- xontel_supermarket/urls.py
+++ xontel_supermarket/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('supermarket/', include('supermarket_app.urls'))
+    path('', include('supermarket_app.urls'))
 ]
