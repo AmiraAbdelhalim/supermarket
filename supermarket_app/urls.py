66e1800043c646535fa1a3096333c7d95569ef74
amira <amira.a.a.hashim@gmail.com>
2020-09-21 21:51:24 +0200

    display all customers, invoices, products

--- supermarket_app/urls.py
+++ supermarket_app/urls.py
@@ -5,5 +5,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns =[
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name="logout"),
-    path('', views.Home),
+    path('', views.Home, name='home'),
+    path('customers/', views.CustomerList, name='customers'),
+    path('products/', views.ProductList, name='products'),
+    path('invoices/', views.InvoiceList, name='invoices'),
+
+
 ]
--- supermarket_app/views.py
+++ supermarket_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Customer, Product, Invoice
 
 # Create your views here
 def Home(request):
     return render(request, 'home.html')
+
+def CustomerList(request):
+    customers = Customer.objects.all()
+    return render(request, 'customer.html', {'customers': customers})
+
+def ProductList(request):
+    products = Product.objects.all()
+    return render(request, 'product.html', {'products': products})
+
+def InvoiceList(request):
+    invoices = Invoice.objects.all()
+    context = {
+        'invoices': invoices,
+    }
+    return render(request, 'invoice.html', context)
